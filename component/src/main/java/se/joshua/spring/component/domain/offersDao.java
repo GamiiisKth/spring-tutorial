40513370fb7cf83271097540df7f53b352d709ae
FAHA58 <Ali-yusha.hasan@konsult.folksam.se>
2016-11-09 16:00:29 +0100

    added other dependencies

---
+++ component/src/main/java/se/joshua/spring/component/domain/offersDao.java
@@ -0,0 +1,13 @@
+package se.joshua.spring.component.domain;
+
+import java.util.List;
+
+/**
+ * @author Ali Yusha {@literal <mailto:Ali.yusha@so4it.com/>}
+ */
+public class offersDao {
+
+    public List<Offer>  getOffer(){
+        return null;
+    }
+}
